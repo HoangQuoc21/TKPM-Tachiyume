3d48091d26c24d5fe88d937d28524d890ed00ddd
HoangQuoc21 <hoangquoc2106@gmail.com>
2024-04-16 19:26:14 +0700

    #3: Chỉnh lại cấu trúc folder một xíu

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello Phong, đây là App khởi tạo của Quốc</Text>
+      <StatusBar style='auto'/>
     </View>
   );
 }
