efff53ecd3deb17b6eb4fa2d398cc032c55b3bba
21120589 <truonganhtuan3092@gmail.com>
2024-04-23 20:42:22 +0700

    add SourceInterface

--- mobile/src/app.tsx
+++ mobile/src/app.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useEffect } from 'react';
+import SourceOne from './factory/SourceOne';
+import SourceInterface from './factory/SourceInterface';
 
 export default function App() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    // const sourceOne: SourceInterface = new SourceOne();
+    // sourceOne.findNovelsByPage(1);
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         try {
+    //             const data = await sourceOne.findNovelsByPage(1)
+    //             console.log(data)
+    //             setData(data);
+    //             setLoading(false);
+    //         } catch (error) {
+    //             setError(error);
+    //             setLoading(false);
+    //         }
+    //     }
+
+    //     fetchData();
+
+    // }, []);
+
     return (
         <View style={styles.container}>
             <Text>Đây là nội dung trong file src/app.tsx</Text>
